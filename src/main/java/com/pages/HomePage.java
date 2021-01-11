5f6efe8cf67ffe5d87d99e41b29767e40f8366b8
Dmitrii_Zolotukhin <dmitrii_zolotukhin@epam.com>
2021-01-11 21:18:28 +0400

    refactoring3_1

--- src/main/java/com/pages/HomePage.java
+++ src/main/java/com/pages/HomePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
@@ -90,6 +91,7 @@ public class HomePage {
 
     @Step
     public void buttonAfterPassword() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"passwordNext\"]/div/button/div[2]")));
         buttonAfterPassword.click();
     }
 
